3c6f069da75a2b8a4d0433400507ed8dfa78770d
MatteusKill <matteuskill@gmail.com>
2026-04-13 10:41:28 -0400

    task04
    Criação da área de avaliação e footer.

--- JS/cardapio.js
+++ JS/cardapio.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-    $(".fa-solid fa-heart").on("click", function(){
-        $("fa-solid fa-heart").toggleClass('active');
+    $(".fa-solid.fa-heart").on("click", function(){
+        $(this).toggleClass('ativo');
     })
 })
